f5e491c25e929516454ea3e5f2b8e02daf45ef61
vsaint1 <vsantos1.dev@gmail.com>
2023-12-07 01:09:39 -0300

    feat: exporting functions

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,21 +1,28 @@
+#include "process/memory.h"
 #include <cstdio>
 #include <jni/jni.h>
 
 // LANG_PACKAGE_CLASSNAME_FUNCTION(args)
 
 extern "C" {
-__declspec(dllexport) void __stdcall Java_test_CTest_hello() { printf("Hello world from c++"); }
-__declspec(dllexport) int Java_test_CTest_sumNumber(int a, int b) { return a + b; }
 
-JNIEXPORT jint JNICALL Java_test_CTest_jniSum(JNIEnv *env, jobject obj, jint a, jint b) { return a + b; }
+ENGINE_API long get_process_id(const char *process_name) {
+  
+  long process_id = g_Memory.get_process_by_name(process_name);
+  g_Memory.~Memory();
 
-JNIEXPORT jlong JNICALL Java_test_CMemory_getProcessId(JNIEnv *p_env,jobject jobj ,jstring process_name) {
-    const char* native_str = p_env->GetStringUTFChars(process_name,nullptr);
-
-  printf("Process name %s \n", native_str);
-
-  p_env->ReleaseStringUTFChars(process_name,native_str);
-  return 12345678910;
+  return process_id;
 }
 
+JNIEXPORT jlong JNICALL Java_test_CMemory_getProcessId(JNIEnv *p_env, jobject jobj, jstring process_name) {
+
+  const char *native_str = p_env->GetStringUTFChars(process_name, nullptr);
+
+  long process_id = g_Memory.get_process_by_name(native_str);
+
+  p_env->ReleaseStringUTFChars(process_name, native_str);
+  g_Memory.~Memory();
+
+  return process_id;
+}
 }
