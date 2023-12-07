188ceec72891100b8c04d0bf1a22d5f4254453ef
vsaint1 <vsantos1.dev@gmail.com>
2023-12-06 23:01:03 -0300

    feat: jni sum example

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,6 +1,12 @@
 #include <cstdio>
+#include <jni/jni.h>
 
 extern "C" {
 __declspec(dllexport) void __stdcall Java_test_CTest_hello() { printf("Hello world from c++"); }
 __declspec(dllexport) int Java_test_CTest_sumNumber(int a, int b) { return a + b; }
+
+
+JNIEXPORT jint JNICALL Java_test_CTest_jniSum(JNIEnv *env, jobject obj, jint a, jint b) {
+    return a + b;
+}
 }
