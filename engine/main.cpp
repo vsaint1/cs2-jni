7b7abfd40c7fd0e5b4a9183a53ca098c85439ab2
vsaint1 <vsantos1.dev@gmail.com>
2023-12-08 00:25:46 -0300

    feat: exporting functions and jni help doc

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,8 +1,27 @@
 #include "process/memory.h"
 #include <cstdio>
 #include <jni/jni.h>
+#include <vector>
+#include "Entity.h"
 
-// LANG_PACKAGE_CLASSNAME_FUNCTION(args)
+
+// LANG_PACKAGE_CLASSNAME_FUNCTION(args,...)
+
+// Ljava/lang/String - String.class
+// V - void
+// F - 32/64 bit float
+// I - 32/64 bit int
+// Z - boolean
+// D - double
+// C - unicode char
+// S - short(int)
+// J - long
+// B - byte
+// [ - one-dimensional array
+// [] - array
+
+//(IZLjava/lang/String;[BC)V
+// void randomFunction(int,boolean,string,byte[])
 
 extern "C" {
 
@@ -37,4 +56,38 @@ JNIEXPORT jlong JNICALL Java_test_CMemory_getModuleBase(JNIEnv *p_env, jobject j
   return res;
 }
 
+JNIEXPORT jobjectArray JNICALL Java_test_CMemory_getEntities(JNIEnv *p_env, jobject jobj) {
+
+
+
+  std::vector<Entity_t> entites = { {"Unknown",123.0f,20} ,{"Unknown", 100.0f, 100}, {"Unknown", 30.0f, 40}, {"Unknown", 50.0f, 80}};
+  
+  jclass entity_class = p_env->FindClass("test/CEntity");
+
+  jmethodID ctor = p_env->GetMethodID(entity_class, "<init>", "(Ljava/lang/String;FI)V");
+
+  if (entity_class == nullptr)
+    return nullptr;
+
+    jobjectArray entities = p_env->NewObjectArray(entites.size(), entity_class, nullptr);
+
+     for (int i = 0; i < entites.size(); i++) {
+        jstring name = p_env->NewStringUTF(entites[i].name.c_str());
+
+        jobject playerObj = p_env->NewObject(entity_class, ctor, name, entites[i].distance, entites[i].health);
+
+        p_env->SetObjectArrayElement(entities, i, playerObj);
+
+        p_env->DeleteLocalRef(name);
+        p_env->DeleteLocalRef(playerObj);
+
+    
+     
+    }
+
+    return entities;
 }
+
+}
+
+
