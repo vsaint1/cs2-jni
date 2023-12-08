6d1a74c80afcd48538ad27dab586e2eece5a09ef
vsaint1 <vsantos1.dev@gmail.com>
2023-12-08 00:25:23 -0300

    test:  getting entities with JNI

--- engine/test/CMemory.java
+++ engine/test/CMemory.java
@@ -4,13 +4,18 @@ import java.io.File;
 
 public class CMemory {
     private static String FILE_NAME = "engine.dll";
-
+ 
     static {
         System.load(System.getProperty("user.dir") + File.separatorChar + "build" + File.separatorChar
                 + File.separatorChar + "Debug" + File.separatorChar + "engine.dll");
     }
 
+    public native CEntity[] getEntities();
+
     public native long getProcessId(String processName);
 
     public native long getModuleBase(String moduleName);
+
+    
+
 }
--- engine/test/Main.java
+++ engine/test/Main.java
@@ -1,16 +1,23 @@
 package test;
 
+
+
 public class Main {
-    
+
     public static void main(String[] args) {
 
-        long pid = new CMemory().
-        getProcessId("cs2.exe");
+        long pid = new CMemory().getProcessId("cs2.exe");
 
         System.out.println("[CLIENT] - " + pid);
 
         long moduleBase = new CMemory().getModuleBase("client.dll");
-                System.out.println("[CLIENT] - " + moduleBase);
+        System.out.println("[CLIENT] - " + moduleBase);
+
+        CEntity[] entities = new CMemory().getEntities();
+
+        for (CEntity entity : entities) {
+            System.out.println(entity.toString());
+        }
 
     }
 }
