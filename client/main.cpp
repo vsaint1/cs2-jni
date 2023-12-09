f30487357b5f4a6a1c08f853888dd655e2c0c701
vsaint1 <vsantos1.dev@gmail.com>
2023-12-09 00:46:49 -0300

    refactor jni

---
+++ client/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "features/globals.h"
+#include "../engine/process/memory.h"
+
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+
+  return 0;
+}
--- engine/main.cpp
+++ engine/main.cpp
@@ -25,22 +25,19 @@
 
 extern "C" {
 
-ENGINE_API long get_process_id(const char *process_name) {
+// ENGINE_API long get_process_id(const char *process_name) {
 
-  long process_id = g_Memory.get_process_by_name(process_name);
-  g_Memory.~Memory();
+//   long process_id = g_Memory.get_process_by_name(process_name);
+//   g_Memory.~Memory();
 
-  return process_id;
-}
+//   return process_id;
+// }
 
-JNIEXPORT jlong JNICALL Java_test_CMemory_getProcessId(JNIEnv *p_env, jobject jobj, jstring process_name) {
+JNIEXPORT jlong JNICALL Java_test_CMemory_getProcessId(JNIEnv *p_env, jobject jobj) {
 
-  const char *native_str = p_env->GetStringUTFChars(process_name, nullptr);
+  long process_id = process.get_procid();
 
-  long process_id = g_Memory.get_process_by_name(native_str);
-
-  p_env->ReleaseStringUTFChars(process_name, native_str);
-  g_Memory.~Memory();
+  process.~Memory();
 
   return process_id;
 }
@@ -48,10 +45,10 @@ JNIEXPORT jlong JNICALL Java_test_CMemory_getProcessId(JNIEnv *p_env, jobject jo
 JNIEXPORT jlong JNICALL Java_test_CMemory_getModuleBase(JNIEnv *p_env, jobject jobj, jstring process_name) {
   const char *native_str = p_env->GetStringUTFChars(process_name, nullptr);
 
-  jlong res = static_cast<jlong>(g_Memory.get_module_base(native_str));
+  jlong res = static_cast<jlong>(process.get_modulebase(native_str));
 
   p_env->ReleaseStringUTFChars(process_name, native_str);
-  g_Memory.~Memory();
+  process.~Memory();
 
   return res;
 }
