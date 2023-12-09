56d6e893e290507d59f6c094e73c8978a022a0e2
vsaint1 <vsantos1.dev@gmail.com>
2023-12-09 00:45:31 -0300

    refactor: memory class

--- engine/process/memory.h
+++ engine/process/memory.h
@@ -3,65 +3,56 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <string>
+#include "handle_hijack.h"
 
 typedef NTSTATUS(WINAPI *pNtReadVirtualMemory)(HANDLE ProcessHandle, PVOID BaseAddress, PVOID Buffer, ULONG NumberOfBytesToRead, PULONG NumberOfBytesRead);
 typedef NTSTATUS(WINAPI *pNtWriteVirtualMemory)(HANDLE Processhandle, PVOID BaseAddress, PVOID Buffer, ULONG NumberOfBytesToWrite, PULONG NumberOfBytesWritten);
 
-class Memory {
+class ENGINE_API Memory {
+private:
+  HANDLE handle = 0;
+  DWORD process_id = 0;
 
-  HANDLE m_Handle = 0;
-  DWORD m_Pid = 0;
+  pNtReadVirtualMemory read_virtual;
+  pNtWriteVirtualMemory write_virtual;
 
-  pNtReadVirtualMemory m_ReadVM;
-  pNtWriteVirtualMemory m_WriteVM;
-
-public:
-  ~Memory() {
-
-    if (m_Handle)
-      CloseHandle(m_Handle);
-  }
-
-  long get_process_by_name(const char *process_name) {
-
-    LOG("process_name: %s", process_name);
-
+  uintptr_t get_process_id(std::string process_name) {
     PROCESSENTRY32 pe;
-    HANDLE valid_ts = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, m_Pid);
-    m_Pid = 0;
+
+    LOG("%s", process_name.c_str());
+    HANDLE valid_ts = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, process_id);
+
+    uintptr_t result = 0;
 
     if (valid_ts != INVALID_HANDLE_VALUE) {
       while (Process32Next(valid_ts, &pe)) {
-        if (!strcmp(process_name, pe.szExeFile)) {
-
-          m_Pid = pe.th32ProcessID;
+        if (!process_name.compare(pe.szExeFile)) {
+          result = pe.th32ProcessID;
           break;
         }
       }
     }
 
+    LOG("%d", result);
     if (valid_ts)
       CloseHandle(valid_ts);
 
-    LOG("pid: %d", m_Pid);
-
-    return m_Pid;
+    return result;
   }
 
-  uintptr_t get_module_base(const char *module_name) {
+  uintptr_t get_module(std::string module_name) {
     MODULEENTRY32 pe;
 
-    LOG("module_name: %s", module_name);
+    LOG("%s", module_name.c_str());
+    HANDLE valid_ts = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, process_id);
 
-    HANDLE valid_ts = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, m_Pid);
-
-    uintptr_t module_base = 0;
+    uintptr_t result = 0;
 
     if (valid_ts != INVALID_HANDLE_VALUE) {
       while (Module32Next(valid_ts, &pe)) {
-        if (!strcmp(module_name, pe.szModule)) {
+        if (!module_name.compare(pe.szModule)) {
 
-          module_base = reinterpret_cast<uintptr_t>(pe.modBaseAddr);
+          result = reinterpret_cast<uintptr_t>(pe.modBaseAddr);
           break;
         }
       }
@@ -70,28 +61,94 @@ public:
     if (valid_ts)
       CloseHandle(valid_ts);
 
-    LOG("module_base: %llu", module_base);
-
-    if (module_base != 0)
-      return module_base;
+    LOG("%d", result);
+    if (result != 0)
+      return result;
 
     return false;
   }
 
+public:
+  Memory(std::string proces_name) {
+    read_virtual = (pNtReadVirtualMemory)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtReadVirtualMemory");
+    write_virtual = (pNtWriteVirtualMemory)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtWriteVirtualMemory");
+
+    process_id = get_process_id(proces_name);
+
+    if (process_id) {
+      PROCESSENTRY32 pe;
+
+      HANDLE valid_ts = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, process_id);
+
+      if (valid_ts != INVALID_HANDLE_VALUE) {
+        while (Process32Next(valid_ts, &pe)) {
+          if (!proces_name.compare(pe.szExeFile)) {
+            handle = hj::HijackExistingHandle(process_id);
+            break;
+          }
+        }
+      }
+
+      if (valid_ts)
+        CloseHandle(valid_ts);
+    }
+  }
+
+  ~Memory() {
+
+    LOG("Destructor called");
+    if (handle)
+      CloseHandle(handle);
+  }
+
+  uintptr_t get_modulebase(std::string module_name) { return get_module(module_name); }
+
+  int get_procid() { return process_id; }
+
   template <typename T> T readv(uintptr_t address) {
     T buffer{};
-    m_ReadVM(m_Handle, (void *)address, &buffer, sizeof(T), 0);
+    read_virtual(handle, (void *)address, &buffer, sizeof(T), 0);
+    return buffer;
+  }
+
+  template <typename T> T writev(uintptr_t address, T value) {
+    write_virtual(handle, (void *)address, &value, sizeof(T), NULL);
+    return value;
+  }
+
+  std::string read_str(uintptr_t address, size_t size) {
+    std::string buffer(size, '\0');
+    read_virtual(handle, (void *)address, &buffer, size, 0);
     return buffer;
   }
 
   bool read_raw(uintptr_t address, void *buffer, size_t size) {
-    ULONG bytes_read;
-    if (m_ReadVM(m_Handle, (void *)address, &buffer, static_cast<ULONG>(size - 1), &bytes_read))
+    SIZE_T bytes_read;
+    if (read_virtual(handle, (void *)address, &buffer, static_cast<ULONG>(size), (PULONG)&bytes_read))
       return bytes_read == size;
 
     return false;
   }
 
+  bool process_opened(std::string process_name) {
+    if (get_process_id(process_name) != 0)
+      return true;
+
+    return false;
+  }
+
+  bool window_focused(std::string target) {
+
+    HWND hwnd = GetForegroundWindow();
+
+    char title[256];
+    GetWindowText(hwnd, title, sizeof(title));
+
+    if (strstr(title, target.c_str()) != nullptr)
+      return true;
+
+    return false;
+  }
 };
 
-inline Memory g_Memory;
+inline Memory process("cs2.exe"); // TODO: change to your desired process game name
