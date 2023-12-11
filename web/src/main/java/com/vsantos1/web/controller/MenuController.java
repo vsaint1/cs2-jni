20f3aabc38bf68a88a09976f4c00feed229d8786
vsaint1 <vsantos1.dev@gmail.com>
2023-12-11 14:31:12 -0300

    feat: misc web config

--- web/src/main/java/com/vsantos1/web/controller/MenuController.java
+++ web/src/main/java/com/vsantos1/web/controller/MenuController.java
@@ -9,26 +9,29 @@ import org.springframework.web.servlet.ModelAndView;
 import com.vsantos1.web.engine.CAimbotConfig;
 import com.vsantos1.web.engine.CEngine;
 import com.vsantos1.web.engine.CEspConfig;
+import com.vsantos1.web.engine.CMiscConfig;
 
 @Controller
 public class MenuController {
 
     @GetMapping("/menu")
-    public ModelAndView menu(CAimbotConfig aimConfig, CEspConfig espConfig) {
+    public ModelAndView menu(CAimbotConfig aimConfig, CEspConfig espConfig,CMiscConfig miscConfig) {
 
         ModelAndView mv = new ModelAndView("views/menu");
         mv.addObject("aimConfig", aimConfig);
         mv.addObject("espConfig", espConfig);
+        mv.addObject("miscConfig", miscConfig);
 
         return mv;
 
     }
 
     @PostMapping("/save-config")
-    public ModelAndView config(@ModelAttribute CAimbotConfig aimConfig, @ModelAttribute CEspConfig espConfig) {
+    public ModelAndView config(@ModelAttribute CAimbotConfig aimConfig, @ModelAttribute CEspConfig espConfig,@ModelAttribute CMiscConfig miscConfig) {
 
         aimConfig.configure();
         espConfig.configure();
+        miscConfig.configure();
 
         return new ModelAndView("redirect:/menu");
 
