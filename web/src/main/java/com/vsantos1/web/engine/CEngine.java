e332e5a4ae312647e6a3483229b0c890d7533b0a
vsaint1 <vsantos1.dev@gmail.com>
2023-12-11 14:31:41 -0300

    java engine config class

--- web/src/main/java/com/vsantos1/web/engine/CEngine.java
+++ web/src/main/java/com/vsantos1/web/engine/CEngine.java
@@ -3,17 +3,41 @@ package com.vsantos1.web.engine;
 import java.io.File;
 
 public class CEngine {
-     private static String FILE_NAME = "engine.dll";
+    private static String FILE_NAME = "engine.dll";
 
     static {
-        System.load(System.getProperty("user.dir") + File.separatorChar + "build" + File.separatorChar
-                + "engine" + File.separatorChar + "Debug" + File.separatorChar + "engine.dll");
-    }
 
+     
+
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            FILE_NAME = "engine.dll";
+        }
+
+        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+            FILE_NAME = "libengine.so";
+        }
+
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            FILE_NAME = "libengine.dylib";
+        }
+
+        File file = new File(System.getProperty("user.dir") + File.separatorChar  + File.separatorChar + FILE_NAME);
+
+        System.out.println("Loading " + file.getAbsolutePath() + "...");
+        if (!file.exists()) {
+            System.out.println("File " + file.getAbsolutePath() + " does not exist");
+
+            System.exit(0);
+        }
+
+        System.load(file.getAbsolutePath());
+        System.out.println("File " + file.getAbsolutePath() + " loaded");
+    }
 
     public native long getProcessId(String processName);
 
     public native long getModuleBase(String moduleName);
 
     public native void start();
+    public native void stop();
 }
